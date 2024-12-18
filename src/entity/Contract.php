b89dfddf966cd1a6aa4d2ca991f5db467861cf17
LilwenGo <pro.lilian.ortega@gmail.com>
2024-12-18 13:56:59 +0100

    Fiing properties and types

--- src/entity/Contract.php
+++ src/entity/Contract.php
@@ -4,40 +4,40 @@ namespace App\Entity;
 use \DateTime;
 class Contract {
     private int $id;
-    private string $vehicule_uuid;
-    private string $customer_uuid;
-    private DateTime $sign_date;
-    private DateTime $loc_begin_date;
-    private DateTime $loc_end_date;
-    private DateTime $returning_date;
+    private string $vehicule_uid;
+    private string $customer_uid;
+    private string $sign_date;
+    private string $loc_begin_date;
+    private string $loc_end_date;
+    private string $returning_date;
     private float $price;
 
     public function getId(): int {
         return $this->id;
     }
     
-    public function getVehicule_uuid(): string {
-        return $this->vehicule_uuid;
+    public function getVehicule_uid(): string {
+        return $this->vehicule_uid;
     }
     
-    public function getCustomer_uuid(): string {
-        return $this->customer_uuid;
+    public function getCustomer_uid(): string {
+        return $this->customer_uid;
     }
     
     public function getSign_date(): DateTime {
-        return $this->sign_date;
+        return new DateTime($this->sign_date);
     }
     
     public function getLoc_begin_date(): DateTime {
-        return $this->loc_begin_date;
+        return new DateTime($this->loc_begin_date);
     }
     
     public function getLoc_end_date(): DateTime {
-        return $this->loc_end_date;
+        return new DateTime($this->loc_end_date);
     }
     
     public function getReturning_date(): DateTime {
-        return $this->returning_date;
+        return new DateTime($this->returning_date);
     }
 
     public function getPrice(): float {
@@ -48,28 +48,40 @@ class Contract {
         $this->id = $id;
     }
     
-    public function setVehicule_uuid(string $uuid): void {
-        $this->vehicule_uuid = $uuid;
+    public function setVehicule_uid(string $uid): void {
+        $this->vehicule_uid = $uid;
     }
     
-    public function setCustomer_uuid(string $uuid): void {
-        $this->customer_uuid = $uuid;
+    public function setCustomer_uid(string $uid): void {
+        $this->customer_uid = $uid;
     }
     
-    public function setSign_date(string $date): void {
-        $this->sign_date = new DateTime($date);
+    public function setSign_date(DateTime|string $date): void {
+        if(is_object($date)) {
+            $date = $date->format('Y-m-d H:i:s');
+        }
+        $this->sign_date = $date;
     }
     
-    public function setLoc_begin_date(string $date): void {
-        $this->loc_begin_date = new DateTime($date);
+    public function setLoc_begin_date(DateTime|string $date): void {
+        if(is_object($date)) {
+            $date = $date->format('Y-m-d H:i:s');
+        }
+        $this->loc_begin_date = $date;
     }
     
-    public function setLoc_end_date(string $date): void {
-        $this->loc_end_date = new DateTime($date);
+    public function setLoc_end_date(DateTime|string $date): void {
+        if(is_object($date)) {
+            $date = $date->format('Y-m-d H:i:s');
+        }
+        $this->loc_end_date = $date;
     }
     
-    public function setReturning_date(string $date): void {
-        $this->returning_date = new DateTime($date);
+    public function setReturning_date(DateTime|string $date): void {
+        if(is_object($date)) {
+            $date = $date->format('Y-m-d H:i:s');
+        }
+        $this->returning_date = $date;
     }
 
     public function setPrice(float $price): void {
