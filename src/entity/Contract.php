b5d41cd914add7e28d55d6c44e3b860ac4641dee
LilwenGo <pro.lilian.ortega@gmail.com>
2024-12-19 09:31:28 +0100

    Changing types

--- src/entity/Contract.php
+++ src/entity/Contract.php
@@ -7,9 +7,9 @@ class Contract {
     private string $vehicule_uid;
     private string $customer_uid;
     private string $sign_date;
-    private string $loc_begin_date;
-    private string $loc_end_date;
-    private string $returning_date;
+    private ?string $loc_begin_date;
+    private ?string $loc_end_date;
+    private ?string $returning_date;
     private float $price;
 
     public function getId(): int {
@@ -28,16 +28,16 @@ class Contract {
         return new DateTime($this->sign_date);
     }
     
-    public function getLoc_begin_date(): DateTime {
-        return new DateTime($this->loc_begin_date);
+    public function getLoc_begin_date(): ?DateTime {
+        return $this->loc_begin_date ? new DateTime($this->loc_begin_date) : null;
     }
     
-    public function getLoc_end_date(): DateTime {
-        return new DateTime($this->loc_end_date);
+    public function getLoc_end_date(): ?DateTime {
+        return $this->loc_end_date ? new DateTime($this->loc_end_date) : null;
     }
     
-    public function getReturning_date(): DateTime {
-        return new DateTime($this->returning_date);
+    public function getReturning_date(): ?DateTime {
+        return $this->returning_date ? new DateTime($this->returning_date) : null;
     }
 
     public function getPrice(): float {
@@ -63,21 +63,21 @@ class Contract {
         $this->sign_date = $date;
     }
     
-    public function setLoc_begin_date(DateTime|string $date): void {
+    public function setLoc_begin_date(DateTime|string|null $date): void {
         if(is_object($date)) {
             $date = $date->format('Y-m-d H:i:s');
         }
         $this->loc_begin_date = $date;
     }
     
-    public function setLoc_end_date(DateTime|string $date): void {
+    public function setLoc_end_date(DateTime|string|null $date): void {
         if(is_object($date)) {
             $date = $date->format('Y-m-d H:i:s');
         }
         $this->loc_end_date = $date;
     }
     
-    public function setReturning_date(DateTime|string $date): void {
+    public function setReturning_date(DateTime|string|null $date): void {
         if(is_object($date)) {
             $date = $date->format('Y-m-d H:i:s');
         }
